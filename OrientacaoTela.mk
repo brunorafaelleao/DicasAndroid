8ff21cf5538d5e37c09409947466473f7886da6c
Bruno Leão <132297721+brunorafaelleao@users.noreply.github.com>
2024-10-02 09:39:55 -0400

    Create OrientacaoTela.mk

---
+++ OrientacaoTela.mk
@@ -0,0 +1,6 @@
+1) Localizar o arquivo AndroidManifest.xml
+-> Em AndroidManifest localizar a tag <activity
+-> Após o atributo android:name=".nomeActivity" colocar o atributo android:screenOrientation="landscape" (nesse caso vai fixar a tela
+na orientação de paisagem
+
+Obs: Para formato de retrato usar "portrait"
